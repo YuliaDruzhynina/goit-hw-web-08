4e82dc3780caf814cfc9cbeb905828eaca5eed10
Poshtaklon <152329594+YuliaDruzhynina@users.noreply.github.com>
2024-05-21 20:36:42 +0300

    Add files via upload

---
+++ consumer.py
@@ -0,0 +1,47 @@
+import pika
+from model import Contact
+import sys
+from mongoengine import connect
+from bson import ObjectId
+
+uri = "mongodb+srv://goitlearn:Goit2023@cluster0.p3tvwqy.mongodb.net/hw-08?retryWrites=true&w=majority&appName=Cluster0"
+connect(host=uri)
+
+def main():
+    credentials = pika.PlainCredentials('guest', 'guest')
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='email_queue', durable=True)
+
+    
+    def callback(ch, method, properties, body):
+        contact_id_str = body.decode('utf-8').strip('"') 
+        try:
+            contact_id = ObjectId(contact_id_str)
+            contact = Contact.objects(id=contact_id).first()
+            if contact:
+                contact.email_sent = True
+                contact.save()
+                print(f"Updated contact {contact_id_str}")
+            else:
+                print(f"Contact with id {contact_id_str} not found")
+        except Exception as e:
+            print(f"Error converting body to ObjectId: {e}")
+
+        print(f"Received {body}")
+
+    #channel.basic_qos(prefetch_count=1)макс.кількість повідомлень, які споживач може взяти 
+    channel.basic_consume(queue='email_queue', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        sys.exit(0)
